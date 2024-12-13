ce2f567835ebfc14add210a69ad7370cf1cbe7bd
Dharmik Khuman <100301204+DharmikKhuman@users.noreply.github.com>
2024-12-14 01:18:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('citySelect').addEventListener('change', function () {
+    const selectedCity = this.value;
+    alert(`You selected ${selectedCity}`);
+    // Additional functionality (e.g., filter workers or redirect) can go here
+  });
+  
